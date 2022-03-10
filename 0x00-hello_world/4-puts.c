ca327c64283d30d20fd9ef91666b4549ef493e6c
Adisa Abdulhakeem <yinkadisa@gmail.com>
2022-03-10 12:23:25 -0800

    puts c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 
+
+
 /**
  *
- *  * Description: main - Prints out a string to stdout.
+ *  * main - Entry point
  *
- *   * Return: 0 if success.
+ *   *
  *
- *    */
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
 int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+			return (0);
 
 }
