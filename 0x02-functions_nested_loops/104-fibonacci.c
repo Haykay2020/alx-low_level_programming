d8b1048d3e05030e79ac68eb5901b7488ed9aaed
Adisa Abdulhakeem <yinkadisa@gmail.com>
2022-03-15 14:18:54 -0700

    98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,13 +11,13 @@ int main(void)
 {
 	long int i, x = 1, y = 2, sum = 0;
 
-	for (i = 0; i < 97; i++)
+	for (i = 0; i < 98; i++)
 	{
 		printf("%lu, ", x);
 		sum = x + y;
 		x = y;
 		y = sum;
-		if (i == 96)
+		if (i == 97)
 			printf("%lu\n", x);
 	}
 	return (0);
