e64dc2655d4afef4c01b021a8a475031139fe04b
Adisa Abdulhakeem <yinkadisa@gmail.com>
2022-03-21 14:02:44 -0700

    main h updated

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
-#ifndef _main_h_
-#define _main_h_
+#ifndef main
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -13,4 +13,5 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
+#define main
 #endif
