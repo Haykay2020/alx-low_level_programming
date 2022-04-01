b60a67e323d656c84feef113ede209a49a5fddf0
Adisa Abdulhakeem <yinkadisa@gmail.com>
2022-04-01 07:29:23 -0700

    prints the largest of three integers.

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,16 +1,9 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
- */
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
-int main(void)
-{
-	int i;
-
-	i = 0;
-	positive_or_negative(i);
-
-	return (0);
-}
+#endif
