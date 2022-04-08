cad10131caa57f4728d95523a6359c0b5d0c0090
Adisa Abdulhakeem <yinkadisa@gmail.com>
2022-04-07 21:11:30 -0700

    ADV TASK 2

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,29 +1,39 @@
 #include "main.h"
 
 /**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
+ * main - This prints multiplication of 2 numbers
+ * @argc: This is the argument count
+ * @argv: This is the argument vector
+ *
+ * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-unsigned long mul;
-int i, j;
+	int i, j;
+	unsigned long num1, num2;
+
 	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
+			if (argv[i][j] < 48 || argv[i][j] > 57)
+			{
+				printf("Error\n");
+				exit(98);
+			}
 		}
-
 	}
-	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", mul);
-return (0);
+
+	num1 = atol(argv[1]);
+	num2 = atol(argv[2]);
+
+	printf("%ld\n", num1 * num2);
+
+	return (0);
 }
